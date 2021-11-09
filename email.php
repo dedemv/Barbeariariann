818e0648ca06c4e8cb1fa2f4de9c7e3a3d074ac4
dedemv <72172418+dedemv@users.noreply.github.com>
2021-11-09 11:40:56 -0300

    Add files via upload

---
+++ email.php
@@ -0,0 +1,12 @@
+<?php
+$nome = $_POST ['nome'];
+$email = $_POST ['email'];
+$mensagem = $_POST ['mensagem'];
+?>
+
+<h1>NOME</h1>
+<?= $nome ?>
+<h1>EMAIL</h1>
+<?= $email ?>
+<h1>MENSAGEM</h1>
+<?= $mensagem ?>
